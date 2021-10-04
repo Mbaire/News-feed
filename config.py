3f3e1de6e5d2b22f7aa65f1bdf1914d316124491
Mbaire <isaackingori68@gmia.com>
2021-10-04 13:24:13 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+class Config:
+    '''
+     General configuration parent class
+    '''
+
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    # SECRET_KEY = 'cd4f457d5b644e25bf5caedcfaf8a484'
+
+# @staticmethod
+# def init_app(app):
+#     pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
